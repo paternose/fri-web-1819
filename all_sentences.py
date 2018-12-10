c1751420d5d65ead16ee438c7d28ceea9083528e
noubair.m@gmail.com <noubair.m@gmail.com>
2018-12-10 09:19:44 -0100

    "added tokens/frequency dict"

--- all_sentences.py
+++ all_sentences.py
@@ -1,22 +1,44 @@
-
-
 import os
-path="/home/insight/Documents/OSY/RechercheWeb/FRI_WEB_2018_2019/Cours/Projet/Data/CACM/"
-os.chdir(path)
-f=open("cacm.all","r")
-l=f.readlines()
-f.close()
+from nltk.tokenize import RegexpTokenizer
+
+
+l = []
+try:
+    path="/home/insight/Documents/OSY/RechercheWeb/FRI_WEB_2018_2019/Cours/Projet/Data/CACM/"
+    os.chdir(path)
+    f=open("cacm.all","r")
+    l=f.readlines()
+    f.close()
+except:
+    None
+try:
+    path="/mnt/f/etudes/OSY/recherche_web/Data/Data/CACM/"
+    os.chdir(path)
+    f=open("cacm.all","r")
+    l=f.readlines()
+    f.close()
+except:
+    None
 sentences=[]
-i=0
-while i<len(l):
 
-    if l[i][0:2] in [".K",".I",".W"]:
-        j=i
-        while j<len(l):
-            if l[j][0]!=".": sentences.append(l[j])
-            j+=1
-        i=j
-    else: i+=1
+reading = False
+for line in l:
+    if line[0]==".":
+        if line[0:2] in [".K",".T",".W"]:
+            reading = True
+        else:
+            reading = False
+    else:
+        if reading:
+            sentences.append(line)
+tokens=dict()
+tokenizer = RegexpTokenizer(r'\w+')
+for sentence in sentences:
+    for token in tokenizer.tokenize(sentence):
+        try:
+            tokens[token.lower()]+=1
 
+        #si le token n'est pas encore dans le dictionnaire
+        except:
+            tokens[token.lower()]=1
 
-print(sentences)
