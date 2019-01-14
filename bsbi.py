9b06cbb7821a367d8db949146a57bab10e1daf7d
noubair.m@gmail.com <noubair.m@gmail.com>
2019-01-14 01:30:10 +0000

    added vectorialSearch

--- bsbi.py
+++ bsbi.py
@@ -3,24 +3,24 @@ from tqdm import tqdm
 
 
 def extractRawLines():
-    path1="/home/insight/Documents/OSY/RechercheWeb/FRI_WEB_2018_2019/Cours/Projet/Data/CACM/"
-    path2="/mnt/f/etudes/OSY/recherche_web/Data/Data/CACM/"
+    path1 = "/home/insight/Documents/OSY/RechercheWeb/FRI_WEB_2018_2019/Cours/Projet/Data/CACM/"
+    path2 = "/mnt/f/etudes/OSY/recherche_web/Data/Data/CACM/"
     raw_lines = []
     print("Loading file ..")
     try:
-        f=open(path1 + "cacm.all","r")
-        raw_lines=f.readlines()
+        f = open(path1 + "cacm.all", "r")
+        raw_lines = f.readlines()
         f.close()
     except:
         None
     try:
-        f=open(path2 + "cacm.all","r")
-        raw_lines=f.readlines()
+        f = open(path2 + "cacm.all", "r")
+        raw_lines = f.readlines()
         f.close()
     except:
         None
     return raw_lines
-    
+
 
 def extractDocs(raw_lines):
     docs = {}
@@ -29,13 +29,13 @@ def extractDocs(raw_lines):
     reading = False
 
     for line in tqdm(raw_lines):
-        if line[0]==".":
+        if line[0] == ".":
             if line[0:2] == ".I":
                 if len(current_doc):
                     docs[i] = current_doc[:]
                     i += 1
                 current_doc = []
-            elif line[0:2] in [".K",".T",".W"]:
+            elif line[0:2] in [".K", ".T", ".W"]:
                 reading = True
             else:
                 reading = False
@@ -45,14 +45,16 @@ def extractDocs(raw_lines):
 
     return docs
 
+
 def getBlock(docs, docIds, length_limit):
-    counter=0
-    block=dict()
-    i=0
+    counter = 0
+    block = dict()
+    i = 0
     for doc in docs:
-        if counter+len(doc)<=length_limit: block[docIds[i]]=doc
-        i+=1
+        if counter + len(doc) <= length_limit: block[docIds[i]] = doc
+        i += 1
     return block
+
 
 def extractTokens(lines):
     ## Takes an array of lines as input, returns an array of tokens (with
@@ -66,17 +68,19 @@ def extractTokens(lines):
     return tokens
 
 
-
 """
 block example: key: documentId, value: lines
 block = {1: ["aaa aaa\n", "bbb\n"], 2: ["ccc cc\n", "ddd\n"],3: ["ccc\n","eee\n"]}
 """
+
+
 def countToken(token, lines):
-    counter=0
+    counter = 0
     tokenizer = RegexpTokenizer(r'\w+')
     for line in lines:
-        counter+=tokenizer.tokenize(line).count(token)
+        counter += tokenizer.tokenize(line).count(token)
     return counter
+
 
 def invertBlock(block):
     invertedIndex = dict()
@@ -86,14 +90,13 @@ def invertBlock(block):
             for token in tokenizer.tokenize(line.lower()):
 
                 try:
-                    invertedIndex[token][documentId]+=1
+                    invertedIndex[token][documentId] += 1
                 except:
                     try:
-                        invertedIndex[token][documentId]=1
-                    except:
-                        invertedIndex[token]=dict()
                         invertedIndex[token][documentId] = 1
-
+                    except:
+                        invertedIndex[token] = dict()
+                        invertedIndex[token][documentId] = 1
 
     return invertedIndex
 
@@ -103,10 +106,8 @@ def createIndex():
     raw_lines = extractRawLines()
     docs_lines = extractDocs(raw_lines)
     index = dict()
-#    index = invertBlock(docs_lines)
+    #    index = invertBlock(docs_lines)
     return index
-
-
 
 
 if __name__ == '__main__':
